5a760be6f7cd6e6b7da54116d2f89eb88e7d4141
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2025-11-12 20:05:51 -0300

    feat: add booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Header from './_components/header';
 import SearchInput from './_components/search-input';
 import banner from '../public/banner.png';
+import BookingItem from './_components/booking-item';
 export default function Home() {
   return (
     <>
@@ -11,6 +12,14 @@ export default function Home() {
     <Image src={banner} alt='Agenda agora' 
     sizes='100vw'
     className='h-auto w-full'/> 
+    <h2 className='text-xs text-foreground font-semibold'>Agendamentos</h2>
+    <BookingItem 
+    serviceName='Corte de Cabelo'
+    barbershopName='Barbearia do Zé'
+    barbershopImageUrl='https://utfs.io/f/7e309eaa-d722-465b-b8b6-76217404a3d3-16s.png'
+    date={new Date()}
+     />
+    
     </div>
     </>
   );
