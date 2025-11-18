df02e1c310e43022fd55750d3e7bc230b5d89b67
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2025-11-18 17:28:39 -0300

    feat: ensure booking can be cancelled

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Header from './_components/header'
 import SearchInput from './_components/search-input'
 import banner from '../public/banner.png'
-import BookingItem from './_components/booking-item'
 import { prisma } from '@/lib/prisma'
 import BarbershopItem from './_components/barbershop-item'
 import Footer from './_components/footer'
@@ -37,17 +36,6 @@ export default async function Home() {
           sizes="100vw"
           className="h-auto w-full"
         />
-
-        <PageSection>
-          <PageSectionTitle>Agendamentos</PageSectionTitle>
-          <BookingItem
-            serviceName="Corte de Cabelo"
-            barbershopName="Barbearia do Zé"
-            barbershopImageUrl="https://utfs.io/f/7e309eaa-d722-465b-b8b6-76217404a3d3-16s.png"
-            date={new Date()}
-            status="confirmed"
-          />
-        </PageSection>
 
         <PageSection>
           <PageSectionTitle>Recomendados</PageSectionTitle>
